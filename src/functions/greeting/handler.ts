7d35883b45d4bd7ab49854a5e1cb31c57df22d0e
AshKemp <kmuraliashwin@gmail.com>
2023-04-27 17:59:30 +0530

    update message for v1

--- src/functions/greeting/handler.ts
+++ src/functions/greeting/handler.ts
@@ -2,7 +2,7 @@ import { successClientReponse } from "@libs/api-gateway";
 const greeting = async (event) => {
   console.log("Event: ", event);
   return successClientReponse({
-    message: "Hello from greeting",
+    message: "Hello from greeting v1",
   });
 };
 
