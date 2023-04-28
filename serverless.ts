9b4a7e36c4b1530aa9b2ed421ab3d0a82f037d28
AshKemp <kmuraliashwin@gmail.com>
2023-04-28 21:03:26 +0530

    update stage v1

--- serverless.ts
+++ serverless.ts
@@ -13,7 +13,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
-    stage: process.env.API_VERSION_STAGE,
+    stage: "v1",
     region: "us-west-2",
     endpointType: "regional",
     apiGateway: {
