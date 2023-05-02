dae22bdb3cb3d4a781a8e45d4c5056982c1a46e1
AshKemp <kmuraliashwin@gmail.com>
2023-05-02 14:11:10 +0530

    add restApiId and restApiRootResourceId

--- serverless.ts
+++ serverless.ts
@@ -19,6 +19,8 @@ const serverlessConfiguration: AWS = {
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
+      restApiId: "03wo6guqe5",
+      restApiRootResourceId: "d1gggbowwh",
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
@@ -40,7 +42,7 @@ const serverlessConfiguration: AWS = {
     //   platform: "node",
     //   concurrency: 10,
     // },
-    stage: `{opt:stage,${process.env.API_VERSION_STAGE}}`,
+    // stage: `{opt:stage,${process.env.API_VERSION_STAGE}}`,
   },
 };
 
