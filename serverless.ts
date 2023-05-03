bfe5befe6e26a570c5e691ad0255c81696d98547
AshKemp <kmuraliashwin@gmail.com>
2023-05-03 14:30:09 +0530

    add serverless-stage-manager v1

--- serverless.ts
+++ serverless.ts
@@ -9,7 +9,11 @@ dotenv.config({ path: ".env.local" });
 const serverlessConfiguration: AWS = {
   service: "serverless-hello-world",
   frameworkVersion: "3",
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  plugins: [
+    "serverless-esbuild",
+    "serverless-offline",
+    "serverless-stage-manager",
+  ],
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
